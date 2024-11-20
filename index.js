187b1a83de6db3b50c5188d652e3a5433accd287
b4xabhishek <b4xabhishek@gmail.com>
2024-11-20 23:10:24 +0530

    format change

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 app.use(cors({
   origin: '*',
   methods: '*',
